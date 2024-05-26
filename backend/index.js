abfbfa77552698527734be42d7de70eecc857091
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2024-05-26 17:58:42 +0530

    added types, validation

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const { createToDo, updateToDo } = require('./types')
 
 const app = express()
 
 app.use(express.json())
 
 app.post('/todo', (req,res)=> {
+    const createPayLoad = req.body
+    const parsedPayLoad = createToDo.safeParse(createPayLoad)
 
+    if(!parsedPayLoad.success) {
+        res.status(411).json({
+            message : 'you sent the wrong inputs'
+        })
+    }
 })
 
 app.get('/todos', (req,res)=> {
@@ -13,7 +21,14 @@ app.get('/todos', (req,res)=> {
 })
 
 app.put('/completed', (req,res)=> {
-    
+    const createPayLoad = req.body
+    const parsedPayLoad = updateToDo.safeParse(createPayLoad)
+
+    if(!parsedPayLoad.success) {
+        res.status(411).json({
+            message : 'you sent the wrong inputs'
+        })
+    }
 })
 
 const PORT = 3000
---
+++ backend/types.js
@@ -0,0 +1,15 @@
+const z = require('zod')
+
+const createToDo = z.object({
+    title: z.string(),
+    description: z.string()
+})
+
+const updateToDo = z.object({
+    id: z.string()
+})
+
+module.exports = {
+    createToDo,
+    updateToDo
+}
